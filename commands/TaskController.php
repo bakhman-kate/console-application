d74a68da700ee8cefa10297921386df0ee6ea23c
bakhman-kate <bakhman.ekaterina@gmail.com>
2016-10-13 22:42:39 +0300

    - Added console command task;

---
+++ commands/TaskController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\commands;
+
+use app\models\Task;
+use Plp\Task\FatalException;
+use Plp\Task\UserException;
+use yii\console\Controller;
+
+class TaskController extends Controller
+{
+    const TASK_LIMIT = 10;    
+    const STATUS_CREATED = 0;
+    const STATUS_IN_PROGRESS = 1;   
+    const STATUS_USER_EXCEPTION = 2;
+    const STATUS_FATAL_EXCEPTION = 3;
+    const STATUS_SUCCESSFULL = 4;
+    
+    public function actionIndex()
+    {
+        $tasks = Task::find()            
+            ->where(['<', 'retries', 3])
+            ->andWhere(['<', 'status', self::STATUS_FATAL_EXCEPTION])
+            ->limit(self::TASK_LIMIT)
+            ->all();
+        
+        foreach($tasks as $task) {
+            $taskClass = $task->task;
+            $taskMethod = $task->action;
+            $taskData = json_decode($task->data, true);
+            $task->updateAttributes(['status' => self::STATUS_IN_PROGRESS]);
+            
+            try {
+                $result = call_user_func(array('Plp\\Task\\'.$taskClass, $taskMethod), $taskData);
+                $this->updateFields($task, $result, self::STATUS_SUCCESSFULL);                           
+            } catch (FatalException $e) {
+                $this->updateFields($task, ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()], self::STATUS_FATAL_EXCEPTION);                                              
+            }catch (UserException $e) {
+                $this->updateFields($task, ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()], self::STATUS_USER_EXCEPTION);                
+            }
+            
+            echo "Task ".$task->id." (".$task->task."::".$task->action.") finished ".$task->finished."\n";
+            echo "Result: ".$task->result."\n";
+        }
+    }
+    
+    protected function updateFields($task, $result, $status) {
+        $task->result = json_encode($result);
+        $task->status = $status;
+        $task->retries += 1;
+        $task->finished = date('Y-m-d H:i:s');
+        $task->save();
+    }
+}
