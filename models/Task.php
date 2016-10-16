89b23dee3e40352281db808f3672672cffd1a42d
bakhman-kate <bakhman.ekaterina@gmail.com>
2016-10-17 00:22:28 +0300

    - Added methods execute and updateAfterExecution;

--- models/Task.php
+++ models/Task.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use Plp\Task\FatalException;
+use Plp\Task\UserException;
 
 /**
  * This is the model class for table "task".
@@ -22,6 +24,11 @@ use Yii;
  */
 class Task extends \yii\db\ActiveRecord
 {
+    const STATUS_CREATED = 0;
+    const STATUS_USER_EXCEPTION = 1;
+    const STATUS_FATAL_EXCEPTION = 2;
+    const STATUS_SUCCESSFULL = 3;
+    
     /**
      * @inheritdoc
      */
@@ -62,5 +69,47 @@ class Task extends \yii\db\ActiveRecord
             'finished' => Yii::t('app', 'Finished'),
             'result' => Yii::t('app', 'Result'),
         ];
+    }
+    
+    public function execute()
+    {
+        $taskClass = "\Plp\Task\\".$this->task;
+        $taskMethod = $this->action;
+        $taskData = json_decode($this->data, true);
+        
+        if(!empty($taskData)) {
+            if(class_exists($taskClass)){
+                if(method_exists($taskClass, $taskMethod)) {
+                    try {
+                        $this->updateAfterExecution(self::STATUS_SUCCESSFULL, call_user_func(array($taskClass, $taskMethod), $taskData));                    
+                    } catch (FatalException $e) { 
+                        $this->updateAfterExecution(self::STATUS_FATAL_EXCEPTION, ["result" => false, "message" => $e->getMessage(), "trace" => $e->getTraceAsString()]);                    
+                    }catch (UserException $e) {
+                        $this->updateAfterExecution(self::STATUS_USER_EXCEPTION, ["result" => false, "message" => $e->getMessage(), "trace" => $e->getTraceAsString()]);
+                    }
+                }
+                else { 
+                    $this->updateAfterExecution(self::STATUS_FATAL_EXCEPTION, ["result" => false, "message" => $taskMethod." doesn't exist in ".$taskClass]);                
+                }                
+            }
+            else {
+                $this->updateAfterExecution(self::STATUS_FATAL_EXCEPTION, ["result" => false, "message" => $taskClass." doesn't exist"]);
+            }        
+        } 
+        else {
+            $this->updateAfterExecution(self::STATUS_FATAL_EXCEPTION, ["result" => false, "message" => "Task data is empty."]);
+        }
+        
+        echo "Task ".$this->id." (".$this->task."::".$this->action.") finished ".$this->finished."\n";
+        echo "Result: ".$this->result."\n";   
+    }
+    
+    private function updateAfterExecution($status, $result) {
+        $this->updateAttributes([
+            'status' => $status,
+            'result' => json_encode($result),
+            'retries' => $this->retries + 1,
+            'finished' => date('Y-m-d H:i:s')
+        ]);
     }
 }
